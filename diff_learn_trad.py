4c389e333e80f5c9daee1576539f1dc5531e6889
Erik Connerty <erikc@email.sc.edu>
2023-12-15 16:41:40 -0500

    result

--- diff_learn_trad.py
+++ diff_learn_trad.py
@@ -21,11 +21,11 @@ def dsigmoid(x):
 
 # Initialize the weights for a 3-layer neural network
 np.random.seed(42)  # For reproducibility
-w1 = np.random.randn(2, 10) 
-w2 = np.random.randn(10, 20)
-w3 = np.random.randn(20, 2) 
-b1 = np.zeros((1, 10))
-b2 = np.zeros((1, 20))
+w1 = np.random.randn(2, 20) / np.sqrt(2)
+w2 = np.random.randn(20, 50) / np.sqrt(10)
+w3 = np.random.randn(50, 2) / np.sqrt(50)
+b1 = np.zeros((1, 20)) 
+b2 = np.zeros((1, 50))
 b3 = np.zeros((1, 2))
 
 # Predicted outputs for the hidden layers
@@ -36,13 +36,13 @@ p2 = np.random.randn(1, 20)
 alpha, beta, gamma, delta = 1.8, 0.001, .1, .0005
 
 # Initial state vector X
-initial_conditions = np.array([[500, 10]])  # Example initial populations
+initial_conditions = np.array([[500, 5]])  # Example initial populations
 
 # Forward pass of the neural network
 def forward_pass(X):
     global w1, w2, w3, b1, b2, b3
-    z1 = tanh(np.dot(X, w1) + b1)
-    z2 = tanh(np.dot(z1, w2) + b2)
+    z1 = relu(np.dot(X, w1) + b1)
+    z2 = relu(np.dot(z1, w2) + b2)
     z3 = np.dot(z2, w3) + b3
     return z1, z2, z3
 
@@ -68,61 +68,67 @@ def update_predicted_outputs(p, z, eta):
     p_new = p + eta * error
     return p_new
 
-"""def update_weights_using_predictions(w, p, X, eta):
-    grad_J_z = p * dtanh(p)
-    grad_J_w = np.dot(X.T, grad_J_z)
-    norm = np.linalg.norm(grad_J_w)
-    norm_grad_J_w = grad_J_w / norm if norm != 0 else grad_J_w
-    w_new = w - eta * norm_grad_J_w
-    return w_new"""
 
 def backpropagate(X, y_true, z1, z2, z3, eta):
-    global w1, w2, w3, b1, b2, b3, p1, p2
+    global w1, w2, w3, b1, b2, b3
 
     # Compute the gradient of the loss w.r.t. the final output
-    grad_loss_z3 = (2 * (z3 - y_true) /2)
+    loss_output = z3 - y_true
+    grad_w3 = np.dot(z2.T, loss_output)
+    grad_b3 = np.sum(loss_output, axis=0, keepdims=True)
 
-    # Backpropagate this gradient to get gradients for w3 and b3 using predicted outputs (p2)
-    grad_w3 = np.dot(p2.T, grad_loss_z3)
-    grad_b3 = np.sum(grad_loss_z3, axis=0, keepdims=True)
-    
-    # Backpropagate through the second hidden layer using the predicted output p2
-    grad_loss_p2 = np.dot(grad_loss_z3, w3.T) * dtanh(p2)
-    grad_w2 = np.dot(p1.T, grad_loss_p2)
-    grad_b2 = np.sum(grad_loss_p2, axis=0, keepdims=True)
-    
-    # Backpropagate through the first hidden layer using the predicted output p1
-    grad_loss_p1 = np.dot(grad_loss_p2, w2.T) * dtanh(p1)
-    grad_w1 = np.dot(X.T, grad_loss_p1)
-    grad_b1 = np.sum(grad_loss_p1, axis=0, keepdims=True)
-
-    # Update the weights and biases
-    w1 -= eta * grad_w1
-    b1 -= eta * grad_b1
-    w2 -= eta * grad_w2
-    b2 -= eta * grad_b2
+    # Update weights and biases for output layer
     w3 -= eta * grad_w3
     b3 -= eta * grad_b3
 
+    # Backpropagate through the second hidden layer
+    grad_loss_z2 = np.dot(loss_output, w3.T) * drelu(z2)
+    grad_w2 = np.dot(z1.T, grad_loss_z2)
+    grad_b2 = np.sum(grad_loss_z2, axis=0, keepdims=True)
 
-# Mean Squared Error (MSE) loss function
+    # Update weights and biases for second hidden layer
+    w2 -= eta * grad_w2
+    b2 -= eta * grad_b2
+
+    # Backpropagate through the first hidden layer
+    grad_loss_z1 = np.dot(grad_loss_z2, w2.T) * drelu(z1)
+    grad_w1 = np.dot(X.T, grad_loss_z1)
+    grad_b1 = np.sum(grad_loss_z1, axis=0, keepdims=True)
+
+    # Update weights and biases for first hidden layer
+    w1 -= eta * grad_w1
+    b1 -= eta * grad_b1
+
+
+
 def mse_loss(y_pred, y_true):
-    return np.mean((y_pred - y_true) ** 2)
+    # Calculate MSE separately for each output and then average
+    mse_per_output = np.mean((y_pred - y_true) ** 2, axis=0)
+    return np.sum(mse_per_output)
+
+def min_max_normalize(data):
+    min_val = np.min(data)
+    max_val = np.max(data)
+    return (data - min_val) / (max_val - min_val)
 
 # Training loop parameters
-epochs = 50
-dt = 1000.0
-learn_rate = 0.01
-hidden_layer_learn = .02
+epochs = 100
+dt = 100.0
+learn_rate = .01
+hidden_layer_learn = .0001
 
 
 # Training loop with loss computation
 losses = []  # To store loss at each epoch
 
 #Generate training data
-data = solve_odeint_step(initial_conditions, dt, alpha, beta, gamma, delta, 1000)
+data = solve_odeint_step(initial_conditions, dt, alpha, beta, gamma, delta, 10000)
 train_data = data[:,:-1]
 labels = data[:,1:]
+
+#Normalize data
+train_data= min_max_normalize(train_data)
+labels= min_max_normalize(labels)
 
 print(labels.shape)
 
@@ -134,27 +140,31 @@ plt.xlabel('Time')
 plt.ylabel('Population')
 plt.title('Training Data')
 plt.legend()
-plt.show()
+plt.savefig('training_data_trad.pdf')
 
+# Training loop with separate updates for predicted layers and weights
 for epoch in range(epochs):
-    for x,y in zip(train_data.T,labels.T):
+    # Second loop to update the weights
+    for x, y in zip(train_data.T, labels.T):
+    # First loop to update the predicted outputs
         x = x.reshape(1, 2)
         y = y.reshape(1, 2)
         z1, z2, z3 = forward_pass(x)
-        p1 = update_predicted_outputs(p1, z1, hidden_layer_learn)
-        p2 = update_predicted_outputs(p2, z2, hidden_layer_learn)
         # Backpropagation to update weights using predicted outputs
         backpropagate(x, y, z1, z2, z3, learn_rate)
-        
+
         # Compute and store the loss
         loss = mse_loss(z3, y)
         losses.append(loss)
 
-            #Print loss for this epoch
-        #print("Epoch: " + str(epoch) + " Loss: " + str(loss))
+    # Optionally, print loss for the epoch
+    print(f"Epoch: {epoch+1}, Loss: {np.mean(losses[-len(train_data.T):])}")
+
+# Plotting code remains unchanged
+
 
 #print(p1)
-print("Lowest Loss: " + np.array(losses).min().astype(str))
+#print("Lowest Loss: " + np.array(losses).min().astype(str))
 # Plot the loss over epochs
 plt.figure(figsize=(10, 5))
 plt.plot(losses, label='MSE Loss')
@@ -162,7 +172,22 @@ plt.xlabel('Epoch')
 plt.ylabel('Loss')
 plt.title('MSE Loss Over Epochs')
 plt.legend()
-plt.show()
+plt.savefig('loss_trad.pdf')
 
+# Plot the neural networks predictions
+predictions = []
+for x, y in zip(train_data.T, labels.T):
+    x = x.reshape(1, 2)
+    y = y.reshape(1, 2)
+    z1, z2, z3 = forward_pass(x)
+    predictions.append(z3)
 
-
+#Plot the predictions
+plt.figure(figsize=(10, 5))
+plt.plot(np.array(predictions)[:,:,0], label='Prey')
+plt.plot(np.array(predictions)[:,:,1], label='Predator')
+plt.xlabel('Time')
+plt.ylabel('Population')
+plt.title('Predictions')
+plt.legend()
+plt.savefig('predictions_trad.pdf')
--- diff_learn_updated.py
+++ diff_learn_updated.py
@@ -21,16 +21,16 @@ def dsigmoid(x):
 
 # Initialize the weights for a 3-layer neural network
 np.random.seed(42)  # For reproducibility
-w1 = np.random.randn(2, 10) / np.sqrt(2)
-w2 = np.random.randn(10, 20) / np.sqrt(10)
-w3 = np.random.randn(20, 2) / np.sqrt(20)
-b1 = np.zeros((1, 10)) 
-b2 = np.zeros((1, 20))
+w1 = np.random.randn(2, 20) / np.sqrt(2)
+w2 = np.random.randn(20, 50) / np.sqrt(20)
+w3 = np.random.randn(50, 2) / np.sqrt(50)
+b1 = np.zeros((1, 20)) 
+b2 = np.zeros((1, 50))
 b3 = np.zeros((1, 2))
 
 # Predicted outputs for the hidden layers
-p1 = np.random.randn(1, 10)
-p2 = np.random.randn(1, 20)
+p1 = np.random.randn(1, 20)
+p2 = np.random.randn(1, 50)
 
 # Lotka-Volterra model parameters
 alpha, beta, gamma, delta = 1.8, 0.001, .1, .0005
@@ -60,68 +60,66 @@ def solve_odeint_step(X, dt, alpha, beta, gamma, delta, n_steps):
     sol = solve_ivp(lotka_volterra_ivp, [0, dt], X.flatten(), args=(alpha, beta, gamma, delta),method='RK45',rtol=1e-13,atol=1e-13)
     return sol.y
 
-
-
 # Update functions for the predicted outputs and weights
 def update_predicted_outputs(p, z, eta):
+    #global w1, w2, w3, b1, b2, b3, p1, p2
     error = z - p
     p_new = p + eta * error
     return p_new
+    """# Update rules for the output layer (traditional approach)
+    loss_output = z3 - y_true
+    grad_p2 = np.dot(w3, loss_output.T) * drelu(p2)
+    p2 -= eta * np.sum(grad_p2) * (p2 - z2)
 
-def clip_gradients(grad, threshold):
-    norm = np.linalg.norm(grad)
-    if norm > threshold:
-        return grad * threshold / norm
-    return grad
+    # Update rules for the second hidden layer
+    loss_hidden_2 = z2 - p2
+    grad_p1 = np.dot(w2, loss_hidden_2.T) * drelu(p1)
+    p1 -= eta * np.sum(grad_p1) * (p1 - z1)
 
-def backpropagate(X, y_true, z1, z2, z3, eta):
+    return p1,p2"""
+
+def update_weights(X, y_true, z1, z2, z3, eta):
     global w1, w2, w3, b1, b2, b3, p1, p2
 
     # Update rules for the output layer (traditional approach)
     loss_output = z3 - y_true
     grad_w3 = np.dot(p2.T, loss_output)
     grad_b3 = np.sum(loss_output, axis=0, keepdims=True)
-    grad_w3 = clip_gradients(grad_w3, 1.0)
     w3 -= eta * grad_w3
     b3 -= eta * grad_b3
 
     # Update rules for the second hidden layer
     loss_hidden_2 = z2 - p2
-    grad_w2 = np.dot(p1.T, loss_hidden_2) * drelu(z2)
+    grad_w2 = np.dot(p1.T, loss_hidden_2) * drelu(p2)
     grad_b2 = np.sum(loss_hidden_2, axis=0, keepdims=True)
-    grad_w2 = clip_gradients(grad_w2, 1.0)
     w2 -= eta * grad_w2
     b2 -= eta * grad_b2
 
     # Update rules for the first hidden layer
     loss_hidden_1 = z1 - p1
-    grad_w1 = np.dot(X.T, loss_hidden_1) * drelu(z1)
+    grad_w1 = np.dot(X.T, loss_hidden_1) * drelu(p1)
     grad_b1 = np.sum(loss_hidden_1, axis=0, keepdims=True)
-    grad_w1 = clip_gradients(grad_w1, 1.0)
     w1 -= eta * grad_w1
     b1 -= eta * grad_b1
 
-    # Add L2 regularization to the weight updates
-    w3 -= eta * (grad_w3 + .1 * w3)
-    w2 -= eta * (grad_w2 + .1 * w2)
-    w1 -= eta * (grad_w1 + .1 * w1)
 
 
-    #print(grad_w1)
-    #print(grad_w2)
-    #print(grad_w3)
+def min_max_normalize(data):
+    return (data - data.min()) / (data.max() - data.min())
 
-
-
-# Mean Squared Error (MSE) loss function
 def mse_loss(y_pred, y_true):
-    return np.mean((y_pred - y_true) ** 2)
+    # Calculate MSE separately for each output and then average
+    mse_per_output = np.mean((y_pred - y_true) ** 2, axis=0)
+    return np.sum(mse_per_output)
+
 
 # Training loop parameters
-epochs = 26
-dt = 20.0
-learn_rate = 5e-6
+epochs = 20
+dt = 100.0
+learn_rate = 1e-5
+learn_rate_decay=1.0
 hidden_layer_learn = .0001
+
 
 
 # Training loop with loss computation
@@ -131,6 +129,9 @@ losses = []  # To store loss at each epoch
 data = solve_odeint_step(initial_conditions, dt, alpha, beta, gamma, delta, 10000)
 train_data = data[:,:-1]
 labels = data[:,1:]
+
+train_data = min_max_normalize(train_data)
+labels = min_max_normalize(labels)
 
 print(labels.shape)
 
@@ -142,26 +143,35 @@ plt.xlabel('Time')
 plt.ylabel('Population')
 plt.title('Training Data')
 plt.legend()
-plt.savefig('training_data.pdf')
+plt.savefig('./output2/training_data.pdf')
 
-# Training loop with separate updates for predicted layers and weights
+
+subset_size = int(train_data.shape[1])  # Size of the random subset
+
+#Train
 for epoch in range(epochs):
-    # Second loop to update the weights
+    # First loop to update the weights
     for x, y in zip(train_data.T, labels.T):
-    # First loop to update the predicted outputs
-        for x, y in zip(train_data.T, labels.T):
-            x = x.reshape(1, 2)
-            z1, z2, z3 = forward_pass(x)
-            # Update predicted outputs for hidden layers
-            p1 = update_predicted_outputs(p1, z1, hidden_layer_learn)
-            p2 = update_predicted_outputs(p2, z2, hidden_layer_learn)
+        # Randomly select a subset of indices for the second loop
+        subset_indices = np.random.choice(train_data.shape[1], subset_size, replace=False)
+        x_subset = train_data[:, subset_indices].T
+        y_subset = labels[:, subset_indices].T
 
+        # Second loop to update the predicted outputs
+        for x2, y2 in zip(x_subset, y_subset):
+            x2 = x2.reshape(1, 2)
+            z1, z2, z3 = forward_pass(x2)
+
+            # Update predicted outputs for hidden layers
+            p1 = update_predicted_outputs(p1,z1, hidden_layer_learn)
+            p2 = update_predicted_outputs(p2, z2, hidden_layer_learn)
 
         x = x.reshape(1, 2)
         y = y.reshape(1, 2)
         z1, z2, z3 = forward_pass(x)
+
         # Backpropagation to update weights using predicted outputs
-        backpropagate(x, y, z1, z2, z3, learn_rate)
+        update_weights(x, y, z1, z2, z3, learn_rate)
 
         # Compute and store the loss
         loss = mse_loss(z3, y)
@@ -169,6 +179,27 @@ for epoch in range(epochs):
 
     # Optionally, print loss for the epoch
     print(f"Epoch: {epoch+1}, Loss: {np.mean(losses[-len(train_data.T):])}")
+    # Plot the neural networks predictions
+    predictions = []
+    for x, y in zip(train_data.T, labels.T):
+        x = x.reshape(1, 2)
+        y = y.reshape(1, 2)
+        z1, z2, z3 = forward_pass(x)
+        predictions.append(z3)
+
+    #Plot the predictions
+    plt.figure(figsize=(10, 5))
+    plt.plot(np.array(predictions)[:,:,0], label='Prey')
+    plt.plot(np.array(predictions)[:,:,1], label='Predator')
+    plt.xlabel('Time')
+    plt.ylabel('Population')
+    plt.title('Predictions')
+    plt.legend()
+    plt.savefig(f'./output2/predictions_{epoch+1}.pdf')
+    plt.close()
+
+    #Decrease learning rate
+    learn_rate = learn_rate*learn_rate_decay
 
 # Plotting code remains unchanged
 
@@ -182,7 +213,7 @@ plt.xlabel('Epoch')
 plt.ylabel('Loss')
 plt.title('MSE Loss Over Epochs')
 plt.legend()
-plt.savefig('loss.pdf')
+plt.savefig('./output2/loss.pdf')
 
 # Plot the neural networks predictions
 predictions = []
@@ -200,7 +231,7 @@ plt.xlabel('Time')
 plt.ylabel('Population')
 plt.title('Predictions')
 plt.legend()
-plt.savefig('predictions.pdf')
+plt.savefig('./output2/final_predictions.pdf')
 
 
 
